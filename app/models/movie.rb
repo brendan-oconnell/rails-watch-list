85031d0657e8a526d7f843a74da5eaa231b436a5
Brendan OConnell <boconnell1@gmail.com>
2022-04-23 11:29:00 +0200

    Finished models

---
+++ app/models/movie.rb
@@ -0,0 +1,10 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+
+  # title is unique (FAILED - 3)
+  # title cannot be blank (FAILED - 4)
+  validates :title, uniqueness: true, presence: true
+
+  # overview cannot be blank (FAILED - 5)
+  validates :overview, presence: true
+end
