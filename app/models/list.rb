f473eb91e5fe2eef9b50c49d954d152b36918c50
Brendan OConnell <boconnell1@gmail.com>
2022-04-26 21:19:34 +0200

    Continued working on front

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,5 @@ class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
   validates :name, presence: true, uniqueness: true
+  validates :image_url, presence: true, uniqueness: true
 end
